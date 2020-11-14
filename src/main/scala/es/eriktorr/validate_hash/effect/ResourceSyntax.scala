ea3bd028a38e2cf6404c308096865efae174f085
Erik Torres <erik.torres@lifullconnect.com>
2020-11-14 23:47:31 +0100

    :fire: Remove dead code

--- src/main/scala/es/eriktorr/validate_hash/effect/ResourceSyntax.scala
+++
@@ -1,9 +0,0 @@
-package es.eriktorr.validate_hash.effect
-
-import cats.effect._
-
-trait ResourceSyntax {
-  implicit class ResourceOps[A](self: A) {
-    def asResource: Resource[IO, A] = Resource.make(IO(self))(_ => IO.unit)
-  }
-}
--- src/main/scala/es/eriktorr/validate_hash/effect/package.scala
+++
@@ -1,3 +0,0 @@
-package es.eriktorr.validate_hash
-
-package object effect extends ResourceSyntax {}
